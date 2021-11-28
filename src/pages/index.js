6b0eba0e979cde8f5b54a4ba1262c0d3878dc1b0
TrevorMicek <miceking@comcast.net>
2021-11-27 20:43:02 -0700

    took CTA comp out

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import React from "react"
 import Layout from "../components/layout/layout"
 import SEO from "../components/layout/seo"
 
-import CTA from '../components/tailwindComponents/CTA'
 import FirstSection from '../components/commonComps/FirstSection'
 import SecondSection from '../components/pages/frontPage/Benefits'
 import info from '../data/frontPage/pageData'
@@ -16,7 +15,7 @@ const IndexPage = () => (
 
 
     <div class="wrapper">
-      <FirstSection comp={CTA} skew="right" bg=" rgba(29, 78, 216, 1)" zTop="4" zBottom="4"  />
+      <FirstSection  skew="right" bg=" rgba(29, 78, 216, 1)" zTop="4" zBottom="4"  />
       <FirstSection {...info[0]} skew="left" bg="rgb(67, 77, 134)" zTop="5" zBottom="5" align="left" />
       <FirstSection {...info[0]} skew="right" bg="rgb(76, 87, 153)" zTop="6" zBottom="6"  />
     </div>
