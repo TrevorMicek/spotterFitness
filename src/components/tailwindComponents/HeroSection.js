d2bd24d8f9188de4d29a7eb484f4c70276e2c5b0
TrevorMicek <miceking@comcast.net>
2022-02-08 07:58:30 -0700

    got rid of stroke x

--- src/components/tailwindComponents/HeroSection.js
+++ src/components/tailwindComponents/HeroSection.js
@@ -19,7 +19,7 @@ export default function Example() {
   const TextStroke = (props) => (
     <div className='flex justify-center'>
     <svg viewBox="0 0 500 50" className={svgStyles.color}>
-  <text x="50" y="50">
+  <text y="50">
     FIND YOUR
     <tspan dx="15" y="50">{props.title}</tspan>
     </text>
