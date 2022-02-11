d533653167aed07f2d6c4a01d00a10b14d8f51c6
TrevorMicek <miceking@comcast.net>
2022-02-11 08:47:10 -0700

    took out app imgs

--- src/components/tailwindComponents/CTA2.js
+++ src/components/tailwindComponents/CTA2.js
@@ -26,7 +26,7 @@ export default function CTA() {
             <div className="-mt-6 aspect-w-5 aspect-h-3 md:aspect-w-2 md:aspect-h-1">
               <img
                 className="transform translate-x-6 translate-y-6 rounded-md object-cover object-left-top sm:translate-x-16 lg:translate-y-20"
-                src="https://tailwindui.com/img/component-images/full-width-with-sidebar.jpg"
+                src=""
                 alt="App screenshot"
                 width="200px"
                 height="100px"
