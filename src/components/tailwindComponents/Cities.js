746362913f207dcf3d3529586762cc67351e8d62
TrevorMicek <miceking@comcast.net>
2022-02-11 08:36:33 -0700

    fixed timeout duration, padding and border

--- src/components/tailwindComponents/Cities.js
+++ src/components/tailwindComponents/Cities.js
@@ -1,15 +1,15 @@
 import React, {useState, useEffect} from 'react'
 import Ticker from 'react-ticker'
 
-const cities = ["Phoenix", "Tuscon", "Denver", "LA", "NYC"]
-const CityScroll = (props) => {
+
+const CityScroll = () => {
   const [tickerSpeed, setTickerSpeed] = useState(1000);
 
   useEffect(() => {
 
     setTimeout(() => {
       setTickerSpeed(10)
-    }, 3000);
+    }, 1500);
 
 
 }, [])
@@ -17,7 +17,7 @@ const CityScroll = (props) => {
     <Ticker speed={tickerSpeed}>
         {({ index }) => (
           <>
-            <p className="mt-4 max-w-3xl text-center text-2xl" style={{color: "rgb(255,244,238)"}}>
+            <p className="max-w-3xl text-center text-2xl" style={{color: "rgb(255,244,238)"}}>
               <span className="mr-16">Phoenix</span>
               <span className="mr-16">Tuscon</span>
               <span className="mr-16">Denver</span>
@@ -34,18 +34,18 @@ export default function Example() {
 
 return (
   <div style={{position: 'relative', top: '0vh', zIndex: '21'}}>
-    <div className="py-0 border-2 border-gray-400">
+    <div className="py-0 border-t-2 border-b-2 border-gray-400 ">
     <div className="max-w-7xl mx-auto text-center py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
     <div className="relative">
           <h2 className="text-center text-sm leading-8 tracking-tight sm:text-4xl" style={{color: "rgb(255,244,238)"}}>
             WE ARE LIVE IN
           </h2>
 
-            <CityScroll cities="Lorem ipsum dolor sit amet consectetur adipisicing elit." />
+            <CityScroll />
 
         </div>
 
-        <h3 className="text-sm tracking-tight sm:text-3xl" style={{color: "rgb(255,244,238)"}}>
+        <h3 className="mt-3 text-sm tracking-tight sm:text-3xl" style={{color: "rgb(255,244,238)"}}>
               COMING TO MORE CITIES SOON
             </h3>
     </div>
