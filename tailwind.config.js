045c88ea436a1e9af529a9732fb466c426464698
TrevorMicek <miceking@comcast.net>
2022-02-14 12:06:02 -0700

    tried adding custom css didn't work. made base font smaller added sm to make it bigger

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,6 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
-    screens: {
-        'phone': '330px',
-        ...defaultTheme.screens,
-     
-    },
   },
   variants: {
     extend: {},
