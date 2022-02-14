dd0669104dac0b2f060426f15fc3a17dafbda19f
TrevorMicek <miceking@comcast.net>
2022-02-14 15:43:30 -0700

    added contact component fixed secondary header

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,11 +4,11 @@ import Layout from "../components/layout/layout"
 import SEO from "../components/layout/seo"
 
 import Main from '../components/pages/ContactPage/contactForm'
-
+import Form from '../components/tailwindComponents/Contact.js'
 const IndexPage = () => (
   <Layout title="Contact Us">
     <SEO title="Home" />
-    <Main />
+    <Form />
   </Layout>
 )
 export default IndexPage
