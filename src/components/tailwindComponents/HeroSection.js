d9eeaebf9fd6e6e89679d0a7f580d421ac6165ac
TrevorMicek <miceking@comcast.net>
2022-03-07 12:54:54 -0700

    added navigator && w/ func

--- src/components/tailwindComponents/HeroSection.js
+++ src/components/tailwindComponents/HeroSection.js
@@ -66,19 +66,19 @@ export default function Example() {
 
   )
   const getMobileOS = () => {
-    var userAgent = navigator.userAgent || navigator.vendor || window.opera || 'undefined';
+    var userAgent = () => navigator && navigator.userAgent || navigator.vendor || window.opera;
 
     // Windows Phone must come first because its UA also contains "Android"
-    if (/windows phone/i.test(userAgent)) {
+    if (/windows phone/i.test(userAgent())) {
         return "Windows Phone";
     }
 
-    if (/android/i.test(userAgent)) {
+    if (/android/i.test(userAgent())) {
         return "https://play.google.com/store/apps/details?id=com.camsilu.spotter";
     }
 
     // iOS detection from: http://stackoverflow.com/a/9039885/177710
-    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+    if (/iPad|iPhone|iPod/.test(userAgent()) && !window.MSStream) {
         return "iOS";
     }
 
