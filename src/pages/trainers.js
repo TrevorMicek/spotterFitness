42807a51c6995056de9e311b839f7d95dc513d76
TrevorMicek <miceking@comcast.net>
2022-03-08 08:26:34 -0700

    added testiomonials for trainers

--- src/pages/trainers.js
+++ src/pages/trainers.js
@@ -23,11 +23,11 @@ const IndexPage = () => {
     <SEO title="Blog" />
     <div class="wrapper">
 
-<FirstSection comp={Features} skew="right" bg="rgba(255,244,238)" borderTop="none" zTop="4" zBottom="4"  />
-<FirstSection comp={CTA} skew="left" bg="rgb(17, 24, 38)" borderTop="none" zTop="5" zBottom="5"  />
-<FirstSection comp={CTA2} skew="right" bg="rgb(231,208,175)" borderTop="none" zTop="6" zBottom="6" pad="15vh"  />
-
-</div>
+      <FirstSection comp={Features} skew="right" bg="rgba(255,244,238)" borderTop="none" zTop="4" zBottom="4"  />
+      <FirstSection comp={CTA} skew="left" bg="rgb(17, 24, 38)" borderTop="none" zTop="5" zBottom="5"  />
+      <FirstSection comp={CTA2} skew="right" bg="rgb(231,208,175)" borderTop="none" zTop="6" zBottom="6" pad="15vh"  />
+      <FirstSection comp={Testimonials}  skew="left" bg="rgb(17, 24, 38)"  zTop="8" zBottom="8" pad="15vh"  />
+    </div>
   </Layout>
   )
   }
