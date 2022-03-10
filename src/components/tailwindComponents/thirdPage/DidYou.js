c020787bca85ea1837a5d1f6be348fe86845883f
TrevorMicek <miceking@comcast.net>
2022-03-10 13:32:42 -0700

    added did you know section, added new contact form for gyms

---
+++ src/components/tailwindComponents/thirdPage/DidYou.js
@@ -0,0 +1,60 @@
+import React from 'react'
+
+
+
+const features = [
+  {
+    name: '1.',
+    num: "72%",
+    description: "of Gym-goers said they would like to return to a facility once it's okay to do so.",
+
+  },
+  {
+    name: '2.',
+    num: "18-34",
+    description:
+      'year olds are now the largest group who frequent the gym.',
+
+  },
+  {
+    name: '3.',
+    num: "81%",
+    description:
+      ' of millennials exercise or would like to.',
+
+  }
+]
+const schedule = require('../../../images/newImg/schedule.jpg')
+export default function Features() {
+
+  return (
+    <div style={{position: 'relative', top: '0', zIndex: '21'}}>
+    <div className="overflow-hidden">
+      <div className="relative max-w-7xl mx-auto py-12 px-4 sm:px-6 md:py-28 lg:px-8">
+
+
+        <div className="relative">
+          <div className="lg:col-span-1">
+            <h2 className="font-serif text-white text-2xl text-center font-bold tracking-tight sm:text-4xl md:mb-12">
+              DID YOU KNOW?
+            </h2>
+          </div>
+          <dl className="relative flex flex-col items-start justify-between md:items-start md:flex-row lg:mt-0 lg:col-span-2 lg:max-w-screen-lg lg:mx-auto">
+            {features.map((feature, i) => (
+              <div key={feature.name}  className="flex flex-row align-start justify-between">
+                <dt>
+
+                  <div className="mt-2 mr-4 text-lg text-white">{feature.name}</div>
+
+                </dt>
+                <dd className="relative text-base text-left text-white md:mx-4"><span className="bold text-4xl">{feature.num} </span>{feature.description}</dd>
+              </div>
+            ))}
+          </dl>
+
+        </div>
+      </div>
+    </div>
+    </div>
+  )
+}
--- src/pages/gyms.js
+++ src/pages/gyms.js
@@ -11,6 +11,7 @@ import CTA2 from '../components/tailwindComponents/thirdPage/CTA2'
 import CTA3 from '../components/tailwindComponents/CTA3'
 import FirstSection from '../components/commonComps/FirstSection'
 import Testimonials from '../components/tailwindComponents/secondPage/Testimonials'
+import DidYou from '../components/tailwindComponents/thirdPage/DidYou'
 import Download from '../components/tailwindComponents/Download'
 import Cities from '../components/tailwindComponents/Cities'
 import Work from '../components/tailwindComponents/Work'
@@ -107,7 +108,8 @@ const TextStroke = (props) => (
       <FirstSection comp={Features} skew="right" bg="rgba(255,244,238)" borderTop="none" zTop="4" zBottom="4"  />
       <FirstSection comp={CTA} skew="left" bg="rgb(17, 24, 38)" borderTop="none" zTop="5" zBottom="5"  />
       <FirstSection comp={CTA2} skew="right" bg="rgb(231,208,175)" borderTop="none" zTop="6" zBottom="6" pad="15vh"  />
-      <FirstSection comp={Testimonials}  skew="left" bg="rgb(17, 24, 38)"  zTop="8" zBottom="8" pad="15vh"  />
+      <FirstSection comp={DidYou}  skew="left" bg="rgb(17, 24, 38)"  zTop="7" zBottom="7" pad="15vh"  />
+
     </div>
 
 
