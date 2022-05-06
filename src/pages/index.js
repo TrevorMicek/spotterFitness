6f2262815f7d27f570c6bd4d6694645c50b8cef5
TrevorMicek <miceking@comcast.net>
2022-05-06 09:20:57 -0600

    added require to img

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Layout from "../components/layout/layout"
 import SEO from "../components/layout/seo"
 
 
-import home from '../images/newImg/graphics/home.png'
+const home = require('../images/newImg/graphics/home.png');
 
 const IndexPage = () => (
     <Layout title="Spotter Fitness" pageLayout={{gridTemplateRows: "[top-gutter] auto [top-padding] auto [first] auto"}}>
